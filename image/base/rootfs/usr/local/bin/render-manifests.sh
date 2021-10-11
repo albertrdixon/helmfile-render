12875e10cda33518ccd51fe3e3e671dc1c390c33
albertrdixon <adixon415@gmail.com>
2021-10-10 21:22:42 -0700

    image/base/rootfs/usr/local/bin/render-manifests: fix target env

--- image/base/rootfs/usr/local/bin/render-manifests.sh
+++ image/base/rootfs/usr/local/bin/render-manifests.sh
@@ -92,10 +92,10 @@ function set_helmfile_environment() {
   unset -v helm_env
 
   if [[ -n "$targets_dir" ]] && [[ -f "$targets_dir/$target.yaml" ]]; then
-    mapfile -t target_env < <(yq e -o j <<<"$targets_dir/$target.yaml" | jq -r 'keys[] as $k | "\($k|gsub("[\.-]+","_")|ascii_upcase)=\(.[$k]|tostring|@sh)"' 2>/dev/null)
+    mapfile -t target_env < <(yq e -o j <"$targets_dir/$target.yaml" | jq -r 'keys[] as $k | "\($k|gsub("[\\.-]+";"_")|ascii_upcase)=\(.[$k]|tostring|@sh)"' 2>/dev/null)
     HELMFILE_ENVIRONMENT+=("${target_env[@]}")
   elif [[ -n "$targets_dir" ]] && [[ -f "$targets_dir/$target.json" ]]; then
-    mapfile -t target_env < <(yq e -o j <<<"$targets_dir/$target.json" | jq -r 'keys[] as $k | "\($kgsub("[\.-]+","_")|ascii_upcase)=\(.[$k]|tostring|@sh)"' 2>/dev/null)
+    mapfile -t target_env < <(yq e -o j <"$targets_dir/$target.json" | jq -r 'keys[] as $k | "\($k|gsub("[\\.-]+";"_")|ascii_upcase)=\(.[$k]|tostring|@sh)"' 2>/dev/null)
     HELMFILE_ENVIRONMENT+=("${target_env[@]}")
   fi
   unset -v target_env
